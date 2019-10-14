15ff6ef4846551fad57e4188e739eb9e0828d0d5
dylan <dylanspin100@hotmail.com>
2019-10-14 10:15:11 +0200

    begin dag
    kleine aanpassingen

--- mod_infor/tmpl/default.php
+++ mod_infor/tmpl/default.php
@@ -13,6 +13,7 @@
 
   $modulePath = JURI::base() . 'modules/mod_infor/';
   $doc->addStyleSheet($modulePath.'css/style.css');
+  //$doc->addScript($modulePath.'js/javascript.js');
 
   $css = "
     .grote_{
@@ -26,10 +27,10 @@
 
 <div class="informatie_">
 <?php
-  for($i=0; $i<=$hoeveel; $i++){
-    echo "<div class='vak_'>test</div>";
+  for($i=0; $i<=$hoeveel-1; $i++){//print de vakken zo veel keer uit als aangegeven is in de back end.
+    echo "<div class='vak_' id='vak$i'></div>";
   }
-  if($params->get('social')){
+  if($params->get('social')){//de social media print
     echo "<div class='social'>";
 
         if(strlen($youtube) > 0){
@@ -52,3 +53,56 @@
   } ?>
 
 </div>
+
+<script>
+var div_ = document.getElementById('vak0');
+console.log(div_);
+dragElement(div_);
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (elmnt) {
+    console.log(elmnt)
+    /* if present, the header is where you move the DIV from:*/
+    elmnt.onmousedown = dragMouseDown;
+  } else {
+    console.log("else");
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    console.log("doet function");
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2)+ "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+</script>
+
+<?php
+ ?>
